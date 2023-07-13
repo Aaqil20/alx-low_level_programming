67b01767b8c9ccf45b5d28fc42ee2e69304b7be5
Aaqil20 <aaqilakoojee01@gmail.com>
2023-07-13 02:56:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,5 @@ ssize_t message_length = sizeof("and that piece of art is useful\" - Dora Korpar
 
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", message_length);
 
-return 1;
+return (1);
 }
