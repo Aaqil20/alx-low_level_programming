a9af9f863afea800d0cd291998b51cb379dc8274
Aaqil20 <aaqilakoojee01@gmail.com>
2023-07-13 02:53:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-    ssize_t message_length = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+ssize_t message_length = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", message_length);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", message_length);
 
-    return 1;
+return 1;
 }
