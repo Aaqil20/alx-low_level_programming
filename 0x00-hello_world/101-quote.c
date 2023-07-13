19cae19cd4fc70e7206336293a79c1abc462098a
Aaqil20 <aaqilakoojee01@gmail.com>
2023-07-13 02:44:37 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+ssize_t ret;
+
+ret = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+if (ret == -1)
+{
+perror("write");
+return (1);
+}
+
+return (1);
+}
