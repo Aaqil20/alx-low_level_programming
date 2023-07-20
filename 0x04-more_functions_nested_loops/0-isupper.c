9efe033d7f50f2ed53857ec6a18e2bc3f59729f9
Aaqil20 <aaqilakoojee01@gmail.com>
2023-07-20 03:28:41 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - Check if a character is a digit
- * @c: The number to be checked
- * Return: 1 for a character that will be a digit or 0 for any else
+ * _isupper - Check if a letter is upper
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
  */
 
-int _isdigit(int c)
+int _isupper(int x)
 
 {
 
-	if (c >= 48 && c <= 57)
+	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
